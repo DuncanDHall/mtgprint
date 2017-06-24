e9c142689df47dce366b89c2b83c97d18345f0e6
Duncan Hall <duncan.hall@students.olin.edu>
2017-06-23 22:16:03 -0400

    added gitignore and starting files

---
+++ start.py
@@ -0,0 +1,9 @@
+#  for images:
+#  http://magiccards.info/query?q=e%3Abe%2Fen&v=card&s=cname&p=1
+#  http://magiccards.info/query?q=e%3Abe%2Fen&s=cname&v=card&p=15
+
+#  only rares:
+#  http://magiccards.info/query?q=r%3Arare+e%3Abe%2Fen&v=card&s=cname
+
+
+
