fa2f821130b126ff51bbcdb26efe7ce5737f4acb
Duncan Hall <duncan.hall@students.olin.edu>
2017-06-27 23:53:19 -0400

    scrape updated to better support set changing

--- scrape.py
+++ scrape.py
@@ -12,16 +12,29 @@ import re
 import urllib.request
 
 # html sources
-beta_rares_url = "http://magiccards.info/query?q=r%3Arare+e%3Abe%2Fen&v=card&s=cname"
-beta_uncommons_url = "http://magiccards.info/query?q=r%3Auncommon+e%3Abe%2Fen&v=card&s=cname"
-beta_commons_url = "http://magiccards.info/query?q=r%3Acommon+e%3Abe%2Fen&v=card&s=cname"
-first_page_urls = [beta_rares_url, beta_uncommons_url, beta_commons_url]
-query_rarity = ["beta rare", "beta uncommon", "beta common"]
+
+# for beta
+# file_name = "beta_cards.csv"
+# rares_url = "http://magiccards.info/query?q=r%3Arare+e%3Abe%2Fen&v=card&s=cname"
+# uncommons_url = "http://magiccards.info/query?q=r%3Auncommon+e%3Abe%2Fen&v=card&s=cname"
+# commons_url = "http://magiccards.info/query?q=r%3Acommon+e%3Abe%2Fen&v=card&s=cname"
+
+# for Amonkhet
+file_name = "amonkhet_cards.csv"
+set_abbreviation = "akh"
+mythics_url = "http://magiccards.info/query?q=r%3Amythic+e%3Aakh%2Fen&v=card&s=cname"
+rares_url = "http://magiccards.info/query?q=r%3Arare+e%3Aakh%2Fen&v=card&s=cname"
+uncommons_url = "http://magiccards.info/query?q=r%3Auncommon+e%3Aakh%2Fen&v=card&s=cname"
+commons_url = "http://magiccards.info/query?q=r%3Acommon+e%3Aakh%2Fen&v=card&s=cname"
+
+
+first_page_urls = [mythics_url, rares_url, uncommons_url, commons_url]
+query_rarity = ["mythic", "rare", "uncommon", "common"]
 queries = zip(query_rarity, first_page_urls)
 
 # re patterns
-card_image_url_pattern = re.compile('<img src="(http://magiccards.info/scans/en/be/\d+.jpg)"')
-card_title_pattern = re.compile('<a href="/be/en/\d+\.html">(.*)</a>')
+card_image_url_pattern = re.compile('<img src="(http://magiccards.info/scans/en/' + set_abbreviation + '/\d+.jpg)"')
+card_title_pattern = re.compile('<a href="/' + set_abbreviation + '/en/\d+\.html">(.*)</a>')
 
 # iterate through queries extracting card names and image links
 collection = []
@@ -30,8 +43,6 @@ for label, url in queries:
     # grab html for all pages associated with that query
     previous_first_card = ""
     page_num = 1
-
-    # import pudb; pu.db
 
     while True:
         # collect all image links and card titles on this page
@@ -42,9 +53,7 @@ for label, url in queries:
 
         # just in case
         if len(card_titles) != len(card_image_links):
-            raise Exception(
-                "page scraped incorrectly: found titles do not match found image urls for " + url + "&p=" + page_num
-            )
+            raise Exception("page scraped incorrectly: found titles do not match found image urls for " + url + "&p=" + str(page_num))
 
         # this site serves the highest available page if we've run out
         if previous_first_card == card_titles[0]:
@@ -58,10 +67,10 @@ for label, url in queries:
 
 
 # store that collection
-writer = csv.writer(open("beta_cards.csv", "w"))
+writer = csv.writer(open(file_name, "w"))
 head = ("Title", "Rarity", "Image URL")
 writer.writerow(head)
 for entry in collection:
     writer.writerow(entry)
 
-print("scrape complete and saved at ./beta_cards.csv\n")
+print("scrape complete and saved at ./" + file_name + "\n")
